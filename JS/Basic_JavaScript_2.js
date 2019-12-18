ca1e26beac1811360505924b064558b5c44e05e7
IsabelFlohr <isabelflohr@gmail.com>
2019-12-18 14:40:59 -0800

    new

---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "First part of sentence, ";
+    sentence += "second part of sentence.";
+    document.getElementById("concantenate").innerHTML = sentence;
+}
