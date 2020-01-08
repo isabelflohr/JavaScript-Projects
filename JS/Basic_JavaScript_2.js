6f872fd1b63223cfc1d54c8e0148751772f8824c
IsabelFlohr <isabelflohr@gmail.com>
2020-01-08 11:53:12 -0800

    edits

--- JS/Basic_JavaScript_2.js
+++ JS/Basic_JavaScript_2.js
@@ -1,4 +1,4 @@
-function myFirstFuntion() {
+function myFirstFunction() {
     var str = "This text is pink!";
     var result = str.fontcolor("pink");
     document.getElementById("concantenate").innerHTML = result;
