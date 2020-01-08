39c940017c814f4de65e2ecc1a90316d5b603806
IsabelFlohr <isabelflohr@gmail.com>
2020-01-08 15:21:38 -0800

    edits

--- JS/Basic_JavaScript_3.js
+++ JS/Basic_JavaScript_3.js
@@ -1,4 +1,4 @@
-function addition_Function() {
-    var addition = 47 + 77 ;
-    document.getElementById("Math").innerHTML = "47 + 77 = " + addition;
+function subtraction_Function() {
+    var subtraction = 8 - 4 ;
+    document.getElementById("Subtracted").innerHTML = "8 - 4 = " - subtraction;
 }
