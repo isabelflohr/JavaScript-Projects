befa9906dd51476599fa5ad9ebeb25b997be1786
IsabelFlohr <isabelflohr@gmail.com>
2020-01-14 01:01:11 -0800

    new

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,34 @@
+document.write(typeof "tree");
+
+function T_or_F() {
+document.getElementById("testing").innerHTML = isNaN ("skiing");
+}
+document.write(3E978);
+document.write(-4E392);
+document.write(80<222);
+console.log(4+10);
+document.write("22" + 2);
+document.write(10 == 10);
+
+X = 44;
+Y = 44;
+document.write(X === Y);
+
+A = "twenty";
+Z = 20;
+document.write(A === Z);
+
+Q = 81;
+U = 1;
+document.write(Q === U);
+
+P = "three";
+I = "seven";
+document.write(P === I);
+
+document.write(4>1 && 8<12);
+document.write(7<8 || 5>2);
+
+function a_func() {
+    document.getElementById("not").innerHTML=!(10>30);
+}
