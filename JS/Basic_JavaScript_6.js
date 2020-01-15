d0357b56297426b56654b277f541023130041fa2
IsabelFlohr <isabelflohr@gmail.com>
2020-01-15 15:20:46 -0800

    edits

---
+++ JS/Basic_JavaScript_6.js
@@ -0,0 +1,16 @@
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short " : "You are tall enough ";
+    document.getElementById("Ride").innerHTML = Can_ride + "to ride";
+}
+
+function subtract_One() {
+    document.getElementById("Nested_Functon").innerHTML = Count();
+    function Count() {
+        var Subtract_From = 100;
+        function Plus_five() {Subtract_From += 5;}
+        Plus_five();
+        return Subtract_From;
+    }
+}
